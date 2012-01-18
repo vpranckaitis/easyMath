07de9d6414dd3d68911bd39090cbf2b788305ee8
Vilius Pranckaitis <vpranckaitis@gmail.com>
2012-01-18 11:11:34 +0000

    Changed prefs.js
    git-svn-id: svn://svn.code.sf.net/p/easymath-addon/code/trunk@5 2dd36246-645b-490d-b3ca-19034c19d9a1

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,1 @@
 pref("extensions.easymath.boolpref", false);
-pref("extensions.easymath.intpref", 0);
-pref("extensions.easymath.stringpref", "A string");
-
-// https://developer.mozilla.org/en/Localizing_extension_descriptions
-pref("extensions.easymath@vpranckaitis.com.description", "chrome://easymath/locale/overlay.properties");
